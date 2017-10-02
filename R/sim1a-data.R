0538cf1b5058d7590d651479731dfd5e0762ff29
Katie Lotterhos <k.lotterhos@gmail.com>
2017-10-02 16:38:06 -0400

    added code to load data

---
+++ R/sim1a-data.R
@@ -0,0 +1,24 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(sim1a)
+#'
+#' @format An object of class \code{"list"}
+#' 
+#' @keywords datasets
+#'
+#' @references NA
+#'
+#' @source 
+#'
+#' @examples
+#' data(sim1a)
+#'
+#' \donttest{}
+"sim1a"
