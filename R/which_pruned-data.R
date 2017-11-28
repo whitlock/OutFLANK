ffa232269fb87df9ab3f291aa923485b256ddd17
Katie Lotterhos <k.lotterhos@gmail.com>
2017-11-28 11:41:28 -0500

    update data

---
+++ R/which_pruned-data.R
@@ -0,0 +1,22 @@
+#' which_pruned
+#'
+#' The indexes of a quasi-independent set of SNPs for use in calculation
+#' of neutral mean FST and df.
+#'
+#' @docType data
+#'
+#' @usage data(which_pruned)
+#'
+#' @format An object of class \code{"list"}
+#' 
+#' @keywords datasets
+#'
+#' @references NA
+#'
+#' @source 
+#'
+#' @examples
+#' data(which_pruned)
+#'
+#' \donttest{}
+"which_pruned"
