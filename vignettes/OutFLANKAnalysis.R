75c154677ce748745b770fcbb09d824673def392
Katie Lotterhos <k.lotterhos@gmail.com>
2018-04-23 19:57:28 -0400

    update vignette

--- vignettes/OutFLANKAnalysis.R
+++ vignettes/OutFLANKAnalysis.R
@@ -5,9 +5,7 @@ library(devtools)
 if (!("qvalue" %in% installed.packages())){TODO}
 if (!("vcfR" %in% installed.packages())){install.packages("vcfR")} 
 
-#if (!("OutFLANK" %in% installed.packages())){devtools::install_github("whitlock/OutFLANK")} ## will need to UNCOMMENT this link for final .Rmd!
-
-devtools::install_github("whitlock/OutFLANK", ref="development", force=TRUE) # will need to DELETE this link for final .Rmd!
+devtools::install_github("whitlock/OutFLANK")
 
 library(OutFLANK)  # outflank package
 library(vcfR)
@@ -52,31 +50,4 @@ OutFLANKResultsPlotter(out_trim , withOutliers = TRUE,
 
 ## ---- fig.width=6--------------------------------------------------------
 hist(out_trim$results$pvaluesRightTail)
-
-## ---- fig.width=6--------------------------------------------------------
-P1 <- pOutlierFinderChiSqNoCorr(my_fst, Fstbar = out_trim$FSTNoCorrbar, 
-                                   dfInferred = out_trim$dfInferred, qthreshold = 0.05, Hmin=0.1)
-head(P1)
-tail(P1)
-# notice how the output is ordered differently
-
-my_out <- P1$OutlierFlag==TRUE
-plot(P1$He, P1$FST, pch=19, col=rgb(0,0,0,0.1))
-points(P1$He[my_out], P1$FST[my_out], col="blue")
-
-hist(P1$pvaluesRightTail)
-# check the P-value histogram for the full set of data
-# if there are outliers, it should look flat with an inflation near 0
-
-## ---- fig.width=7--------------------------------------------------------
-plot(P1$LocusName[P1$He>0.1], P1$FST[P1$He>0.1],
-     xlab="Position", ylab="FST", col=rgb(0,0,0,0.2))
-  points(P1$LocusName[my_out], P1$FST[my_out], col="magenta", pch=20)  
-
-## ------------------------------------------------------------------------
-data("muts")
-muts[muts$prop>0.1,]
-
-## ------------------------------------------------------------------------
-sessionInfo()
 
